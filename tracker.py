2a8905c65de720ec02f55790560f8e2cc577dd26
A3zooz <hmoda201@gmail.com>
2025-07-21 15:09:43 +0300

    Refactor text style for tracking failure message in tracker.py

--- tracker.py
+++ tracker.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
             (x, y, w, h) = [int(x) for x in bounding_box]
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
         else:
-            cv2.putText(frame, "Tracking failure detected", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+            cv2.putText(frame, "Tracking failure detected", (100, 100), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 2)
         cv2.imshow("Tracking", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
